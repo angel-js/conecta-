b23d8cdfa3b2db73f81cc176c8d791a53cb0ff83
angel-js <angelserrano143@gmail.com>
2022-11-20 00:57:17 -0300

    First Steps & Database ready

---
+++ ficha/models.py
@@ -0,0 +1,260 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Comentario(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    fecha_comentario = models.DateField()
+    hora_comentario = models.TimeField()
+    estado = models.CharField(db_column='Estado', max_length=20)  # Field name made lowercase.
+    comentario = models.CharField(db_column='Comentario', max_length=300)  # Field name made lowercase.
+    id_funcionario = models.ForeignKey('Funcionario', models.DO_NOTHING, db_column='id_funcionario')
+
+    class Meta:
+        managed = False
+        db_table = 'comentario'
+
+
+class Comuna(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    nombre_region = models.CharField(max_length=35)
+    id_region = models.OneToOneField('Region', models.DO_NOTHING, db_column='id_region')
+
+    class Meta:
+        managed = False
+        db_table = 'comuna'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Familiar(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    relacion_paciente = models.CharField(max_length=30)
+    id_usuario = models.OneToOneField('Usuario', models.DO_NOTHING, db_column='id_usuario')
+    id_rol = models.OneToOneField('Rol', models.DO_NOTHING, db_column='id_rol')
+    id_comuna = models.OneToOneField(Comuna, models.DO_NOTHING, db_column='id_comuna')
+
+    class Meta:
+        managed = False
+        db_table = 'familiar'
+
+
+class Ficha(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    id_comentario = models.ForeignKey(Comentario, models.DO_NOTHING, db_column='id_comentario')
+
+    class Meta:
+        managed = False
+        db_table = 'ficha'
+
+
+class Funcionario(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    cargo = models.CharField(max_length=30)
+    id_usuario = models.OneToOneField('Usuario', models.DO_NOTHING, db_column='id_usuario')
+    id_rol = models.OneToOneField('Rol', models.DO_NOTHING, db_column='id_rol')
+    id_comuna = models.OneToOneField(Comuna, models.DO_NOTHING, db_column='id_comuna')
+
+    class Meta:
+        managed = False
+        db_table = 'funcionario'
+
+
+class Ingreso(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    fecha_comentario = models.DateField()
+    hora_comentario = models.TimeField()
+    id_paciente = models.ForeignKey('Paciente', models.DO_NOTHING, db_column='id_paciente')
+    id_patologia = models.ForeignKey('Patologia', models.DO_NOTHING, db_column='id_patologia')
+
+    class Meta:
+        managed = False
+        db_table = 'ingreso'
+
+
+class Paciente(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    sexo_biologico = models.CharField(max_length=20)
+    id_usuario = models.OneToOneField('Usuario', models.DO_NOTHING, db_column='id_usuario')
+    id_rol = models.OneToOneField('Rol', models.DO_NOTHING, db_column='id_rol')
+    id_comuna = models.OneToOneField(Comuna, models.DO_NOTHING, db_column='id_comuna')
+    id_sintoma = models.OneToOneField('Sintomas', models.DO_NOTHING, db_column='id_sintoma')
+
+    class Meta:
+        managed = False
+        db_table = 'paciente'
+
+
+class Patologia(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    nombre_patologia = models.CharField(max_length=35)
+
+    class Meta:
+        managed = False
+        db_table = 'patologia'
+
+
+class Region(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    nombre_region = models.CharField(max_length=35)
+
+    class Meta:
+        managed = False
+        db_table = 'region'
+
+
+class Rol(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    nombre_rol = models.CharField(max_length=20)
+    lectura = models.TextField()  # This field type is a guess.
+    escritura = models.TextField()  # This field type is a guess.
+    borrar = models.TextField()  # This field type is a guess.
+    crear = models.TextField()  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'rol'
+
+
+class Sintomas(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    nombre_sintoma = models.CharField(max_length=35)
+
+    class Meta:
+        managed = False
+        db_table = 'sintomas'
+
+
+class Usuario(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    rut = models.CharField(unique=True, max_length=15)
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+    sexo = models.CharField(max_length=10)
+    email = models.CharField(max_length=80)
+    contrasenia = models.CharField(max_length=80)
+    id_rol = models.OneToOneField(Rol, models.DO_NOTHING, db_column='id_rol')
+
+    class Meta:
+        managed = False
+        db_table = 'usuario'
