5bbedb83a20e874de5262d960bee80cafd30008a
angel-js <angelserrano143@gmail.com>
2022-11-23 10:51:45 -0300

    Ajuste de cambios

--- ficha/urls.py
+++ ficha/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='lista_paciente'),
     path('iniciarsesion/', views.iniciarsesion, name='iniciar_sesion'),
-    path('registrarse/', views.registartse, name='registrarse'),
+    path('registrarse/', views.registarse, name='registrarse'),
     path('main/', views.main, name='main'),
     path('mainFuncionario/', views.mainFuncionario, name='mainFuncionario'),
     path('ficha/', views.ficha, name='ficha'),
--- ficha/views.py
+++ ficha/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def home(request):
@@ -8,9 +12,17 @@ def home(request):
 def iniciarsesion(request):
     return render(request, "iniciar_sesion.html")
 
-
-def registartse(request):
+def registarse(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            message.success(request, f'Usuario {username} creado!')
+        else:
+            form = UserCreationForm()
+        context = {'form': form}
     return render(request, "registrarse.html")
+
 
 def main(request):
     return render(request, "main.html")
